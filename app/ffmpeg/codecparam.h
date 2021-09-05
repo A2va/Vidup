44af6c16957cf0539825fcd36a1cad846831e767
A2va <49582555+A2va@users.noreply.github.com>
2021-09-05 22:04:22 +0200

    Format file

--- app/ffmpeg/codecparam.h
+++ app/ffmpeg/codecparam.h
@@ -12,41 +12,40 @@ extern "C"
 class CodecParam
 {
 public:
-// Set 
-// Video parameters
-void width(int width);
-void height(int height);
-void time_base(AVRational time_base);
-void framerate(AVRational framerate);
-void pix_fmt(AVPixelFormat pix_fmt);
-void bit_rate(int64_t bit_rate);
-void video_codec_id(enum AVCodecID  codec_id);
+    // Set
+    // Video parameters
+    void width(int width);
+    void height(int height);
+    void time_base(AVRational time_base);
+    void framerate(AVRational framerate);
+    void pix_fmt(AVPixelFormat pix_fmt);
+    void bit_rate(int64_t bit_rate);
+    void video_codec_id(enum AVCodecID codec_id);
 
-// Audio parameters
-void sample_rate(int sample_rate);
-void channel_layout(uint64_t channel_layout);
-void sample_fmt(AVSampleFormat sample_fmt);
-void audio_codec_id(enum AVCodecID  codec_id);
+    // Audio parameters
+    void sample_rate(int sample_rate);
+    void channel_layout(uint64_t channel_layout);
+    void sample_fmt(AVSampleFormat sample_fmt);
+    void audio_codec_id(enum AVCodecID codec_id);
 
-// Get
-// Video parameters
-int width();
-int height();
-AVRational time_base();
-AVRational framerate();
-AVPixelFormat pix_fmt();
-int64_t bit_rate();
-enum AVCodecID video_codec_id();
+    // Get
+    // Video parameters
+    int width();
+    int height();
+    AVRational time_base();
+    AVRational framerate();
+    AVPixelFormat pix_fmt();
+    int64_t bit_rate();
+    enum AVCodecID video_codec_id();
 
-// Audio parameters
-int sample_rate();
-uint64_t channel_layout();
-AVSampleFormat sample_fmt();
-enum AVCodecID audio_codec_id();
+    // Audio parameters
+    int sample_rate();
+    uint64_t channel_layout();
+    AVSampleFormat sample_fmt();
+    enum AVCodecID audio_codec_id();
 
-int thread();
-void thread(int thread);
-
+    int thread();
+    void thread(int thread);
 
 private:
     // Video parameters
@@ -57,14 +56,14 @@ private:
     AVPixelFormat pix_fmt_;
     int64_t bit_rate_;
     enum AVCodecID video_codec_id_;
-    
+
     // Audio parameters
     int sample_rate_;
     uint64_t channel_layout_;
     AVSampleFormat sample_fmt_;
     enum AVCodecID audio_codec_id_;
 
-    int thread_=0;
+    int thread_ = 0;
 };
 
 #endif
